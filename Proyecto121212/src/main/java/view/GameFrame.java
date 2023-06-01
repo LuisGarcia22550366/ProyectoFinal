e3dc6f697bca3c314e7562a420257a3612b1428c
LuisGarcia22550366 <124327925+LuisGarcia22550366@users.noreply.github.com>
2023-05-31 22:17:16 -0600

    Add files via upload

---
+++ Proyecto121212/src/main/java/view/GameFrame.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author luis3
+ */
+public class GameFrame extends JFrame{
+    private static final long serialVersionUID = 1L;
+    
+	
+	GameFrame(){
+		GamePanel panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+}
---
+++ Proyecto121212/src/main/java/view/SnakeGame.java
@@ -0,0 +1,10 @@
+package view;
+
+public class SnakeGame {
+
+    public static void main(String[] args) {
+        new GameFrame();
+        SnakeGame snakeGame = new SnakeGame();
+        
+    }
+}
